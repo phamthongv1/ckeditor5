7c453562f19813e08bd795e03a430ef0ff1d6c2a
Thong Pham <phamthongv1@gmail.com>
2021-11-26 10:21:45 +0700

    custom build classic build

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -1,106 +1,96 @@
 /**
- * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-
-// The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
-
-export default class ClassicEditor extends ClassicEditorBase {}
-
-// Plugins to include in the build.
-ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
-];
-
-// Editor configuration.
-ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'uploadImage',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
-};
+ import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+ import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+ import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder.js';
+ import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter.js';
+ import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+ import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+ import Image from '@ckeditor/ckeditor5-image/src/image.js';
+ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+ import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+ import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+ import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+ import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+ import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+ import Link from '@ckeditor/ckeditor5-link/src/link.js';
+ import List from '@ckeditor/ckeditor5-list/src/list.js';
+ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+ import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
+ import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
+ import Table from '@ckeditor/ckeditor5-table/src/table.js';
+ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+ import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+ 
+ class Editor extends ClassicEditor {}
+ 
+ // Plugins to include in the build.
+ Editor.builtinPlugins = [
+	 Autoformat,
+	 Bold,
+	 CKFinder,
+	 CKFinderUploadAdapter,
+	 CloudServices,
+	 Essentials,
+	 FontColor,
+	 Image,
+	 ImageCaption,
+	 ImageStyle,
+	 ImageToolbar,
+	 ImageUpload,
+	 Indent,
+	 Italic,
+	 Link,
+	 List,
+	 Paragraph,
+	 SpecialCharacters,
+	 SpecialCharactersEssentials,
+	 Table,
+	 TableToolbar,
+	 TextTransformation,
+	 TodoList
+ ];
+ 
+ // Editor configuration.
+ Editor.defaultConfig = {
+	 toolbar: {
+		 items: [
+			 'bold',
+			 'italic',
+			 'fontColor',
+			 '|',
+			 'bulletedList',
+			 'todoList',
+			 'numberedList',
+			 'insertTable',
+			 '|',
+			 'specialCharacters',
+			 'imageUpload',
+			 'CKFinder'
+		 ]
+	 },
+	 language: 'en',
+	 image: {
+		 toolbar: [
+			 'imageTextAlternative',
+			 'imageStyle:inline',
+			 'imageStyle:block',
+			 'imageStyle:side'
+		 ]
+	 },
+	 table: {
+		 contentToolbar: [
+			 'tableColumn',
+			 'tableRow',
+			 'mergeTableCells'
+		 ]
+	 }
+ };
+ 
+ export default Editor;
+ 
